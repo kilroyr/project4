b311c8241c7cc79922323a3c8bbcd685632f4e4b
Raphael <raphaellim29@gmail.com>
2024-02-09 09:17:15 +0800

    fixed navbar showing up twice

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,9 @@
 import LoginForm from "../../components/LoginForm/LoginForm"
-import NavBar from "../../components/NavBar/NavBar";
+// import NavBar from "../../components/NavBar/NavBar";
 
 export default function AuthPage( { setUser } ) {
   return (
     <>
-      <NavBar />
       <LoginForm setUser={setUser} />
     </>
   );
