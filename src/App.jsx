950f4bce5c1a42d04280322eba7c60ec013bbc06
Raphael <raphaellim29@gmail.com>
2024-02-09 08:22:13 +0800

    added routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 import Homepage from "./pages/HomePage/HomePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import Navbar from "./components/NavBar/NavBar";
+// import BuildPage from "./pages/BuildPage/BuildPage";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Homepage />
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="*"
+          element={<Homepage />}
+        />
+        {/* <Route
+          path="/build"
+          element={<BuildPage />}
+        /> */}
+        <Route
+          path="/signup"
+          element={<AuthPage />}
+        />
+        <Route
+          path="/profile"
+          element={<ProfilePage />}
+        />
+      </Routes>
     </>
   );
 }
